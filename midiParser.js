51515b9cb3e6cc4bda78426ffdd22633a9b2a989
Zack Kollar <rallokkcaz@gmail.com>
2017-09-14 17:52:56 -0700

    Almost to chunk loading...

--- midiParser.js
+++ midiParser.js
@@ -1,22 +1,24 @@
 'use strict';
 
+console.clear();
+
 function MIDIParser(url) {
     url = url || undefined;
     if(url) this.loadData(url);
 }
 
-MIDIParser.prototype.popBytes = function(amount, type) {
+Array.prototype.popBytes = function(amount, type) {
     type = type || 'integer';
 
     var bytes;
     switch(type) {
         case 'hex':
-            bytes = this.state_buffer.splice(0, amount).map(function(byte) {
+            bytes = this.splice(0, amount).map(function(byte) {
                 return byte.toString(16);
             }).join('');
         break;
         case 'integer':
-            bytes = this.state_buffer.splice(0, amount).map(function(byte) {
+            bytes = this.splice(0, amount).map(function(byte) {
                 return byte.toString(16);
             }).join('');
             bytes = parseInt(bytes, 16);
@@ -26,25 +28,67 @@ MIDIParser.prototype.popBytes = function(amount, type) {
     return bytes;
 };
 
+MIDIParser.prototype.popBytes = function(amount, type) {
+    return this.stateBuffer.popBytes(amount, type);
+};
+
 MIDIParser.prototype.resetBufferState = function() {
-    this.state_buffer = this.buffer.slice();
+    this.stateBuffer = this.buffer.slice();
 }
 
 MIDIParser.prototype.getHeaderAndVerify = function() {
-    var headerStart = this.popBytes(4, 'hex');
+    var headerStart = this.popBytes(4, 'hex'); // MThd '4d546864'
     if(headerStart !== '4d546864') throw 'Invalid midi file!';
+
+    // Pop the header chunksize off (4 bytes), we know it's 6 bytes always.
+    this.popBytes(4);
 
     this.fileInfo = {
         format: this.popBytes(2),
         trackChunks: this.popBytes(2),
         division: this.popBytes(2)
     };
+}
 
-    this.resetBufferState();
+MIDIParser.prototype.getTrackChunk = function() {
+    var trackChunkStart = this.popBytes(4, 'hex'); // MTrk '4d54726b'
+    if(trackChunkStart !== '4d54726b') throw 'Error parsing broken track chunk...';
+    var chunkSize = this.popBytes(4);
+    var trackEvents = this.stateBuffer.splice(0, chunkSize);
+
+    var notes = [];
+
+    while(trackEvents.length-1 > 0) {
+        var variableLengthValue = trackEvents.popBytes(1);
+        if(variableLengthValue >= 128) variableLengthValue |= trackEvents.popBytes(1) << 8;
+
+        var midiEvent = Math.abs(~trackEvents.popBytes(1) >> 1);
+
+        // Is note on/off?
+        if(midiEvent >= 0x80 && midiEvent <= 0x9F) {
+            var note = {};
+
+            note.channel = (midiEvent - 0x80) + 1;
+            note.noteNumber = Math.abs(~trackEvents.popBytes(1) >> 1);
+            note.noteVelocity = Math.abs(~trackEvents.popBytes(1) >> 1);
+
+            notes.push(note);
+        } else {
+            trackEvents.pop(2);
+        }
+    }
+
+    return notes;
 }
 
 MIDIParser.prototype.callParse = function() {
     this.getHeaderAndVerify();
+    var notes = this.getTrackChunk();
+    while(notes) {
+        console.log(notes);
+        notes = this.getTrackChunk();
+    }
+    this.resetBufferState();
     this.parseFunc(this.buffer);
 };
 
@@ -62,7 +106,7 @@ MIDIParser.prototype.loadData = function(url) {
         var data = midiRequest.response;
         if (data) {
             this.buffer = [].slice.call(new Uint8Array(data));
-            this.state_buffer = this.buffer.slice();
+            this.stateBuffer = this.buffer.slice();
             window.dispatchEvent(new Event('data-loaded'));
         }
     }.bind(this);
@@ -75,4 +119,6 @@ window.onload = function() {
     p.parse(function(buffer) {
 
     });
-}
+};
+
+// Array.prototype.popBytes = undefined;
